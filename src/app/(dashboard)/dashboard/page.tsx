d03cf2989ce02d2c79dffc745ec991c30c0475fa
hiraaaku <muthiawidya.fahira@gmail.com>
2025-06-04 06:53:24 +0700

    feat: update dashboard layout with ContainerHeader and improved styling

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,10 +1,11 @@
+import { ContainerHeader } from "@/features/dashboard/components/container.header";
 import { ResumeCard } from "@/features/dashboard/home/components/resume.card";
 
 export default function DashboardPage() {
     return (
-        <div className="p-8">
-            <h2 className="text-2xl font-bold mb-1">Dashbaord</h2>
-            <div className="grid grid-cols-3 gap-4">
+        <div className="p-4 sm:p-8">
+            <ContainerHeader title="Dashboard" breadcrumbs={[{label: "Dashboard", href: "/dashboard"}, {label: "Home", href: "#"}]} />
+            <div className="grid sm:grid-cols-3 gap-4 my-8">
                 <ResumeCard label="This Month Order" value={100} updatedAt="2021-01-01" />
                 <ResumeCard label="This Month Web Order" value={100} updatedAt="2021-01-01" />
                 <ResumeCard label="This Month Web Order Rupiah" value={100} updatedAt="2021-01-01" />
